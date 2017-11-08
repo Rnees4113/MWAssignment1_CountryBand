757a4b8f0f94605eae21b5a3a440d8081ab4f4d2
Rnees4113 <31734415+Rnees4113@users.noreply.github.com>
2017-11-07 18:00:12 -0600

    Add files via upload
    Added all php files.

---
+++ boots_n_beer_band/public_html/info_page.php
@@ -0,0 +1,70 @@
+﻿<!-- Matt Rockhold -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/ryan_nees.css">
+  <title>Boots N' Beer: Information Page</title>
+</head>
+<body>
+  <header>
+    <h1>The Boots N' Beer Band</h1>
+  </header>
+  <nav>
+    <ul>
+      <li>
+        <a href="about_page.html">About Us</a>
+      </li>
+      <li>
+        <a href="album_page.html">Albums</a>
+      </li>
+      <li>
+        <a href="index.html">Index</a>
+      </li>
+      <li>
+        <a href="log-in_page.html">Log-in</a>
+      </li>
+      <li>
+        <a href="schedule_page.html">Schedule</a>
+      </li>
+      <li>
+        <a href="signup_page.html">Sign up</a>
+      </li>
+      <li>
+        <a href="store_page.html">Merchandise</a>
+      </li>
+    </ul>
+  </nav><br>
+  <article>
+    <h2>Information Page</h2>
+    <p>This is an info page to get to know all of the band members of this great country group, Boots N' Beer.</p>
+  </article><br>
+  <article>
+    <h2>Meet the Band</h2>
+  </article><br>
+  <article>
+    <h3>Joe Jacks</h3><img src="img/my_face.jpg" alt="Image of Joe Deere">
+    <p>Joe Jacks is the lead singer of the band. Upon that, he also plays the mandolin. Joe is from Murray, IA and he enjoys
+    wearing a good pair of boots and blue jeans and watching football on Sundays.</p>
+  </article><br>
+  <article>
+    <h3>Randy Raccoon</h3><img src="img/ben_face.png" alt="Image of Randy Raccoon">
+    <p>Randy Raccoon is the lead guitarist and back-up vocals. He plays acoustic, electric, and steel guitar, and also banjo,
+    and fiddle! Randy's favorite country star is Charlie Daniel's and he loves watching the Grand Ole Opry.</p>
+  </article><br>
+  <article>
+    <h3>Denver Denim</h3><img src="img/chase_face.png" alt="Image of Denver Denim">
+    <p>Denver Denim is the lead bass and back-up vocals. He also plays harmonica! Denver's favorite movie is the Blue's
+    Brothers because it's hilarious and it has great music in it too!</p>
+  </article><br>
+  <article>
+    <h3>Jake Grit</h3><img src="img/duck_kid_page.png" alt="Image of Jake Grit">
+    <p>Last on this roster is Jake Grit. Jake is the percussionist and rhythm of the group, so he plays the drums, piano, and
+    even the bongos! Jake says that he has playing drums ever since he was a kid, whether it was when he bought his first pair
+    of drums or when he was beating on the trash can outside, he just felt this rhythm inside!</p>
+  </article><br>
+  <footer>
+    &copy; 2017 by Boots 'N Beer
+  </footer>
+</body>
+</html>
---
+++ boots_n_beer_band/public_html/signup_page.php
@@ -0,0 +1,56 @@
+﻿<!DOCTYPE html>
+<!-- Will Roberts -->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/ryan_nees.css">
+  <title>Boots N' Beer: Sign-up</title>
+</head>
+<body>
+  <header>
+    <h1>The Boots N' Beer Band</h1>
+  </header>
+  <nav>
+    <ul>
+      <li>
+        <a href="about_page.html">About Us</a>
+      </li>
+      <li>
+        <a href="album_page.html">Albums</a>
+      </li>
+      <li>
+        <a href="index.html">Index</a>
+      </li>
+      <li>
+        <a href="info_page.html">Members' Information</a>
+      </li>
+      <li>
+        <a href="log-in_page.html">Log-in</a>
+      </li>
+      <li>
+        <a href="schedule_page.html">Schedule</a>
+      </li>
+      <li>
+        <a href="store_page.html">Merchandise</a>
+      </li>
+    </ul>
+  </nav><br>
+  <article>
+    <h2>Sign Up Page</h2>
+    <p>By creating an account, you will be able to keep a shopping cart of merchandise you intend on purchasing if you leave
+    the webiste. You will also be notified whenever information on any of the pages have been updated.</p>
+    <form>
+      <pre>
+First Name: <input type="text" name="firstname">
+Last Name:  <input type="text" name="lastname">
+Username:   <input type="text" name="username">
+Password:   <input type="text" name="password">
+<input type="submit" value="Submit">
+</pre>
+    </form>
+  </article><br>
+  <footer>
+    &copy; 2017 by Boots 'N Beer
+  </footer>
+</body>
+</html>
