55e50560f97fb920c573c28b2a992ff7bec25918
Rnees4113 <31734415+Rnees4113@users.noreply.github.com>
2017-11-07 18:15:34 -0600

    Add files via upload
    Added footer.php and navigation.php

---
+++ boots_n_beer_band/resources/navigation.php
@@ -0,0 +1,27 @@
+<h1>Boots N' Beer Band</h1>
+<ul>
+      <li>
+        <a href="index.html">Index</a>
+      </li>
+      <li>
+        <a href="about_page.html">About Us</a>
+      </li>
+      <li>
+        <a href="album_page.html">Albums</a>
+      </li>
+      <li>
+        <a href="info_page.html">Members' Information</a>
+      </li>
+      <li>
+        <a href="log-in_page.html">Log-in</a>
+      </li>
+      <li>
+        <a href="schedule_page.html">Schedule</a>
+      </li>
+      <li>
+        <a href="signup_page.html">Sign up</a>
+      </li>
+      <li>
+        <a href="store_page.html">Merchandise</a>
+      </li>
+    </ul>
